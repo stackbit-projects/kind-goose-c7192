632614eb791eae1f7f8404a92b0384b88c3fa163
Stackbit Code Editor <projects@stackbit.com>
2021-04-30 21:19:31 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,8 @@ import SectionActions from './SectionActions';
 
 export default class HeroSection extends React.Component {
     render() {
-        let section = _.get(this.props, 'section', null);
+        let section2 = _.get(this.props, 'section', null);
+        const section = _.cloneDeep(section2)
         let padding_top = _.get(section, 'padding_top', null) || 'medium';
         let padding_bottom = _.get(section, 'padding_bottom', null) || 'medium';
         let align_x = _.get(section, 'align', null) || 'center';
